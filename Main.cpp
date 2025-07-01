7460f8e775f91f78fc001ba93d8ab794581a472a
chandyego <chandler.juego@wsu.edu>
2025-07-01 14:52:57 -0700

    process window escape IO

--- Main.cpp
+++ Main.cpp
@@ -2,10 +2,12 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-	glViewport(0, 0, width, height);
-}
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+// settings
+const unsigned int WIDTH = 800;
+const unsigned int HEIGHT = 600;
 
 int main() {
 	glfwInit();
@@ -13,7 +15,8 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(800, 600, "NoobOpenGL", NULL, NULL);
+	// Window Init
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "NoobOpenGL", NULL, NULL);
 	if (window == NULL) {
 		std::cout << "Failed to create OpenGL window" << std::endl;
 		glfwTerminate();
@@ -22,20 +25,41 @@ int main() {
 
 	glfwMakeContextCurrent(window);
 
+	// GLAD init -- load openGL function pointers
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
 
-	glViewport(0, 0, 800, 600);
+	// Viewport init
+	glViewport(0, 0, WIDTH, HEIGHT);
 
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+	// event loop
 	while (!glfwWindowShouldClose(window)) {
+		processInput(window);
+
+		// render
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// swap buffers and poll IO events
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
 	glfwTerminate();
 	return 0;
+}
+
+// window resize callback
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
 }
