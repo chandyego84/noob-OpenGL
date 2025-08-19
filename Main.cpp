425b1453d52321b1078fa428a562554d96f37895
chandyego <chandler.juego@wsu.edu>
2025-08-19 10:51:51 -0700

    two triangles, one with a different color on each vertex, another with fluctuating color shade

--- Main.cpp
+++ Main.cpp
@@ -10,25 +10,28 @@ void processInput(GLFWwindow* window);
 const unsigned int WIDTH = 800;
 const unsigned int HEIGHT = 600;
 const unsigned int NUM_TRIANGLES = 2;
-const unsigned int FLOATS_PER_TRIANGLE = 9; // vertex floats (x,y,z) for each triangle
+const unsigned int FLOATS_PER_TRIANGLE = 18; // vertex floats (x,y,z) + color floats (r,g,b) = 6 * 3 = 18/Triangle
 
 const char* vertexShaderSource = "#version 330 core\n"
-	"layout (location = 0) in vec3 aPos;\n"
-	"out vec4 vertexColor;\n" // color output for fragment 
+	"layout (location = 0) in vec3 aPos;\n" // position variable with attribute position 0
+	"layout (location = 1) in vec3 aColor;\n" // position variable with attribute position 1
+
+	"out vec3 vertexColor;\n" // output a color fragment shader
 
 	"void main()\n"
 	"{\n"
 	"	gl_Position = vec4(aPos, 1.0);\n"
-	"	vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n" // dark red color
+	"	vertexColor = aColor;\n"
 	"}\0";
 
 const char* fragmentShaderSource1 = "#version 330 core\n"
+	"in vec3 vertexColor;\n" // input variable from the vertex shader (same name and type)
+
 	"out vec4 FragColor;\n"
-	"in vec4 vertexColor;\n" // input variable from the vertex shader (same name and type
 	
 	"void main()\n"
 	"{\n"
-	"	FragColor = vertexColor;\n"
+	"	FragColor = vec4(vertexColor, 1.0);\n"
 	"}\0";
 
 // uniforms: another way to pass data from our app on the CPU to shaders on the GPU
@@ -58,17 +61,29 @@ int main() {
 		vertices.push_back(-0.5f + xOff);
 		vertices.push_back(-0.5f);
 		vertices.push_back(0.0f);
+		// color (R)
+		vertices.push_back(1.0f);
+		vertices.push_back(0.0f);
+		vertices.push_back(0.0f);
 
 		// bottom right
 		vertices.push_back(0.5f + xOff);
 		vertices.push_back(-0.5f);
+		vertices.push_back(0.0f);
+		// color (G)
+		vertices.push_back(0.0f);
+		vertices.push_back(1.0f);
 		vertices.push_back(0.0f);
 
 		// top
 		vertices.push_back(0.0f + xOff);
 		vertices.push_back(0.5f);
 		vertices.push_back(0.0f);
-	}
+		// color (B)
+		vertices.push_back(0.0f);
+		vertices.push_back(0.0f);
+		vertices.push_back(1.0f);
+	}	
 
 	// Window Init
 	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "NoobOpenGL", NULL, NULL);
@@ -94,13 +109,13 @@ int main() {
 	// VERTEX SHADER - processing on the vertex attributes
 	int success;
 	char infoLog[512];
-	unsigned int vertexShader;
+	unsigned int vertexShader, vertexShaderBasic;
 
+	// create vertex shader
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	// bind shader code to shader obj
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
 	glCompileShader(vertexShader);
-
 	// check for shader errors
 	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
 	if (!success) {
@@ -115,7 +130,6 @@ int main() {
 	fragmentShader1 = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader1, 1, &fragmentShaderSource1, NULL);
 	glCompileShader(fragmentShader1);
-
 	// check for shader errors
 	glGetShaderiv(fragmentShader1, GL_COMPILE_STATUS, &success);
 	if (!success) {
@@ -126,6 +140,12 @@ int main() {
 	// fragment2
 	fragmentShader2 = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader2, 1, &fragmentShaderSource2, NULL);
+	glCompileShader(fragmentShader2);
+	glGetShaderiv(fragmentShader2, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(fragmentShader2, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT2::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
 
 	// SHADER PROGRAM (obj) - final linked version of multiple shaders combined
 	unsigned int shaderProgram1, shaderProgram2; // fragment shaders
@@ -136,7 +156,7 @@ int main() {
 	glAttachShader(shaderProgram1, fragmentShader1);
 	glLinkProgram(shaderProgram1);
 
-	glGetProgramiv(shaderProgram1, GL_COMPILE_STATUS, &success);
+	glGetProgramiv(shaderProgram1, GL_LINK_STATUS, &success);
 	if (!success) {
 		glGetProgramInfoLog(shaderProgram1, 512, NULL, infoLog);
 		std::cout << "ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n" << infoLog << std::endl;
@@ -147,6 +167,7 @@ int main() {
 	glAttachShader(shaderProgram2, fragmentShader2);
 	glLinkProgram(shaderProgram2);
 	
+	// delete shaders since we have shader programs already
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader1);
 	glDeleteShader(fragmentShader2);
@@ -167,29 +188,43 @@ int main() {
 	glGenBuffers(2, VBO); // VBO - vertex buffer - memory on the GPU to store vertex 
 
 	// SET UP FOR VAO1
-	// bind 
 	glBindVertexArray(VAO[0]);
 
 	// copy vertices array in a vertex buffer
 	glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
 	// static draw - data set once (vertices don't change) and used many times
-	glBufferData(GL_ARRAY_BUFFER, vertices.size() / NUM_TRIANGLES * sizeof(float), vertices.data(), GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, FLOATS_PER_TRIANGLE * sizeof(float), vertices.data(), GL_STATIC_DRAW);
 
 	// LINKING VERTEX SHADER ATTRIBUTES TO VERTEX DATA -- specifying how openGL should interpret vertex data before rendering
-	// specifying 'position' attribute in vertex shader w/ layout (location = 0)
-	// vertex attribute is a vec3, each vec coordinate is a float. thus, each vertex is 12B apart
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+		// specifying 'position' attribute in vertex shader w/ layout (location = 0)
+		// vertex attribute is a vec3, each vec coordinate is a float. thus, each vertex is 12B apart
+	// position attribute
+	glVertexAttribPointer(0,		// layout(location = 0) -- position
+		3,							// 3 componenets (x,y,z)
+		GL_FLOAT,					// type
+		GL_FALSE,					// don't normalize
+		6 * sizeof(float),			// stride (each vertex is 6 floats)
+		(void*)0);					// offset -- of this attribute (position)
 	glEnableVertexAttribArray(0);
+	
+	// color attribute
+	glVertexAttribPointer(1, 
+		3, 
+		GL_FLOAT, 
+		GL_FALSE, 
+		6 * sizeof(float), 
+		(void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
-	glBindVertexArray(0); // no need to unbind as we bind to a different VAO in next lines
+	glBindVertexArray(0); // we are unbinding hre, but we do not actually need to unbind as we bind to a different VAO in next lines
 	glBindBuffer(GL_ARRAY_BUFFER, 0); // also no need to unbind here, but let's keep them for learning's sake
 
 	// SET UP FOR VAO2
 	glBindVertexArray(VAO[1]);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
-	glBufferData(GL_ARRAY_BUFFER, vertices.size() / NUM_TRIANGLES * sizeof(float), vertices.data() + FLOATS_PER_TRIANGLE, GL_STATIC_DRAW);
-	
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glBufferData(GL_ARRAY_BUFFER, FLOATS_PER_TRIANGLE * sizeof(float) * sizeof(float), vertices.data() + FLOATS_PER_TRIANGLE, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 	
 	glBindVertexArray(0);
@@ -212,9 +247,10 @@ int main() {
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		// Draw second shape
+			// we are setting uniform ("ourColor") value in the CPU instead of passing from one shader to the next
 		glUseProgram(shaderProgram2);
 		float timeValue = glfwGetTime();
-		float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+		float greenValue = sin(timeValue) / 2.0f + 0.5f;
 		int vertexColorLocation = glGetUniformLocation(shaderProgram2, "ourColor");
 		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 
